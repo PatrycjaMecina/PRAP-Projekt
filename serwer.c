65945e2a715a054b563447fc3c2694daeb749912
Patrycja Mecina <mpatrycja0308@gmail.com>
2018-02-20 19:14:04 +0100

    Dodanie mozliwosci realizcji funkcji sumowania i odejmowania

--- serwer.c
+++ serwer.c
@@ -3,6 +3,17 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+
+struct lista_wiazana {
+    struct lista_wiazana *nastepny_element;
+    int liczba;
+};
+
+int suma(struct lista_wiazana *lista);
+
+int roznica(struct lista_wiazana *lista);
 
 int main(int argc , char *argv[])
 {
@@ -30,6 +41,9 @@ int main(int argc , char *argv[])
     puts("SERWER TCP: Czekam na polaczenie...");
     c = sizeof(struct sockaddr_in);
     int pid;
+    struct lista_wiazana *listaWiazana;
+    struct lista_wiazana *staraListaWiazana;
+    staraListaWiazana = NULL;
     while (1) {
         //Akceptuj polaczenie przychodzace
         new_socket = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
@@ -39,18 +53,41 @@ int main(int argc , char *argv[])
         }
         puts("Polaczenie zaakceptowane");
         //close(socket_desc);
-        pid = fork();
-        if (pid < 0) {
+        pid = fork(); //tworzenie nowego procesu, jezeli funkcja fork() zwróci 0 to jestesmy w procesie dziecka, jezeli zwróci wartosc > 0 to jestesmy w procesie macierzystym
+        if (pid < 0) { // warunek sprawdzajacy czy udalo sie stworzyc nowy proces
             perror("ERROR in new process creation");
             _exit(1);
         }
-        if (pid == 0) {
+        if (pid == 0) { // warunek sprawdzajacy czy jestesmy w procesie dziecka
             char client_message[3000];  // bufor wiadomosci z poleceniami od klienta
             char message_result[3000];  // bufor odpowiedzi dla klienta
+            int liczba = 0;
+            int wynik = 0;
             sprintf(message_result, "Serwer odebral dane");
             while ((read_size = recv(new_socket, client_message, 3000, 0)) > 0) { //sprawdzanie czy przyszło polecenie do serwera
                 puts(client_message);
-                write(new_socket, message_result, strlen(message_result));  //wysylanie odpowiedzi do klienta
+                liczba = atoi(client_message);  // zamiana odebranych danych na liczbe
+                if(liczba != 0) {
+                    listaWiazana = malloc (sizeof(struct lista_wiazana)); // zaalokowanie pamieci o rozmiarze listy wiazanej dla wskaznika
+                    listaWiazana->liczba = liczba;  // przypisanie liczby do zmiennej w liscie wiazanej
+                    listaWiazana->nastepny_element = staraListaWiazana; // wskazanie na kolejny element w liscie
+                    staraListaWiazana = listaWiazana;
+                }
+                if (strstr(client_message, "suma") != NULL){    //sprawdzanie czy klient wyslal komende do zsumowania liczb
+                    wynik = suma(listaWiazana);
+                    snprintf(message_result, 3000, "Wynik sumowania: %d\n", wynik);
+                    write(new_socket, message_result, strlen(message_result)); //wysylanie odpowiedzi do klienta
+                } else if (strstr(client_message, "roznica") != NULL){  //sprawdzanie czy klient wyslal komende do odejmowania liczb
+                    wynik = roznica(listaWiazana);
+                    snprintf(message_result, 3000, "Wynik odejmowania: %d\n", wynik);
+                    write(new_socket, message_result, strlen(message_result)); //wysylanie odpowiedzi do klienta
+                } else if (client_message[0] == 't') {  //sprawdzanie czy klient wyslal litere potwierdzajaca zakonczenie dzialania
+                    kill(getpid(), SIGKILL);    //zabicie procesu
+                } else {
+                    char message[] = "Potwierdzenie odebrania danych przez serwer\n";
+                    write(new_socket, message, strlen(message));    //wysylanie odpowiedzi do klienta
+                }
+                //write(new_socket, message_result, strlen(message_result));  //wysylanie odpowiedzi do klienta
                 memset (client_message, 0, sizeof (client_message));    // wypelnienie zerami bufora wiadomosci z polecenimi od klienta
             }
             if(read_size == 0) {
@@ -60,4 +97,28 @@ int main(int argc , char *argv[])
             close(new_socket);
         }
     }
+}
+
+int suma(struct lista_wiazana *lista) {
+    struct lista_wiazana *wskaznik = lista;
+    int suma = 0;
+    while (wskaznik != NULL) {
+        suma = suma + wskaznik->liczba;
+        wskaznik = wskaznik->nastepny_element;
+    }
+    return suma;
+}
+
+int roznica(struct lista_wiazana *lista) {
+    struct lista_wiazana *wskaznik = lista;
+    int suma = 0;
+    while (wskaznik != NULL) {
+        suma = suma - wskaznik->liczba;
+        wskaznik = wskaznik->nastepny_element;
+        if(wskaznik->nastepny_element == NULL) {
+            suma = wskaznik->liczba + suma;
+            break;
+        }
+    }
+    return suma;
 }
